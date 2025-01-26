0d24fee58e3227307241a3672fadd770cf3e6010
sanwalsulehri <sanwalsulehri077@gmail.com>
2025-01-26 22:11:59 +0500

    Add some AmazingFeature issue #15

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,48 @@ import { lazy, useEffect, useState } from "react";
 import { NoteContext } from "./contexts/note.context";
 import { DEFAULT_RELAYS, initialNote, NostrNote } from "./shared/constants";
 import { RelaysContext } from "./contexts/relays.context";
-import { getRelaysFromLocalStorage, setRelaysInLocalStorage } from "./shared/utils";
+import {
+  getRelaysFromLocalStorage,
+  setRelaysInLocalStorage,
+} from "./shared/utils";
 import { trackEvent } from "./shared/mixpanel.util";
 
-const HomePage = lazy(() => import('./pages/Home.page'));
+const HomePage = lazy(() => import("./pages/Home.page"));
 
 const existingRelays = getRelaysFromLocalStorage();
 
 function App() {
-  const [ note, setNote ] = useState<NostrNote>(initialNote);
-  const [ relays, setRelays ] = useState<string[]>(existingRelays && existingRelays.length > 0 ? existingRelays : DEFAULT_RELAYS);
+  const [note, setNote] = useState<NostrNote>(initialNote);
+  const [relays, setRelays] = useState<string[]>(
+    existingRelays && existingRelays.length > 0
+      ? existingRelays
+      : DEFAULT_RELAYS
+  );
 
   useEffect(() => {
     trackEvent("IMAGE_GENERATED", note.postId);
-  }, [note])
+  }, [note]);
 
   return (
-    <RelaysContext.Provider value={{
-      relays,
-      setRelays: (relays: string[]) => {
-        setRelaysInLocalStorage(relays);
-        setRelays(relays);
-      },
-    }}>
-      <NoteContext.Provider value={{
-        note,
-        setNote,
-      }}>
+    <RelaysContext.Provider
+      value={{
+        relays,
+        setRelays: (relays: string[]) => {
+          setRelaysInLocalStorage(relays);
+          setRelays(relays);
+        },
+      }}
+    >
+      <NoteContext.Provider
+        value={{
+          note,
+          setNote,
+        }}
+      >
         <HomePage />
       </NoteContext.Provider>
     </RelaysContext.Provider>
   );
 }
 
-export default App
+export default App;
